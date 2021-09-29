458084213934d65dc5a5c1ae4e9859bd54ba2536
Dmitry Shvetsov <shvetsovdm@gmail.com>
2021-09-29 19:44:38 +0200

    [task-management] add validation to get-tasks-filter

--- task-management/src/tasks/dto/get-tasks-filter.dto.ts
+++ task-management/src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,8 +1,14 @@
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { TaskStatus } from '../task';
 
 type SearchQuery = string;
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status: TaskStatus;
+
+  @IsOptional()
+  @IsString()
   q: SearchQuery;
 }
