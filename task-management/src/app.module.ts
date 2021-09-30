f021ae2d118cc882ea2fd19da97451f12953bd34
Dmitry Shvetsov <shvetsovdm@gmail.com>
2021-09-30 13:39:43 +0200

    [task-management] add db dependencies

--- task-management/src/app.module.ts
+++ task-management/src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const ormModule = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 54321,
+  database: 'task_management',
+  username: 'postgres',
+  password: 'postgres',
+  autoLoadEntities: true,
+  synchronize: true,
+});
 
 @Module({
-  imports: [TasksModule],
+  imports: [ormModule, TasksModule],
 })
 export class AppModule {}
