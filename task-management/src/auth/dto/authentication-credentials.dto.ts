2d5489244b6635fe7857adcdf3494bb4619f5e60
Dmitry Shvetsov <shvetsovdm@gmail.com>
2021-10-09 11:19:24 +0200

    [task-management] add validation to auth credent

--- task-management/src/auth/dto/authentication-credentials.dto.ts
+++ task-management/src/auth/dto/authentication-credentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+import { PASSWORD } from '../../lib/rexp';
+
 export class AuthenticationCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(16)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @MaxLength(32)
+  @Matches(PASSWORD, { message: 'password is too weak' })
   password: string;
 }
