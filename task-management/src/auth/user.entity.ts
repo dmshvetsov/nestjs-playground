67c1a9db210254c46928bd1b3204764bbc3cf80e
Dmitry Shvetsov <shvetsovdm@gmail.com>
2021-10-09 11:38:38 +0200

    [task-management] add unique index to username

--- task-management/src/auth/user.entity.ts
+++ task-management/src/auth/user.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
@@ -6,6 +6,7 @@ export class User {
   id: string;
 
   @Column()
+  @Index('user_uniq_username', { unique: true })
   username: string;
 
   @Column()
